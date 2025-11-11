469a1f3068e00733aea3346aeeb3d3cca7f55255
Keenan Hom <keenan.hom@me.com>
2025-11-10 22:55:57 -0500

    Update work on stage 2 embedder

--- embed_stage2.py
+++ embed_stage2.py
@@ -1,11 +1,33 @@
+from typing import Union, List
+
+import numpy as np
+import torch
+
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from transformers import BertTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModel, RobertaTokenizer, RobertaModel
 
 from database import Posting
 
+_EMBED_MODEL = 'roberta-base'
 
-def fetch_postings_text(database_url):
+def _clean_text(text: str) -> str:
+    '''
+    Remove special characters, whitespace, etc. from the raw text.
+    
+    Currently the following operations are performed:
+    - Remove newlines ('\n')
+    - Remove extra whitespace and replace with single space
+    '''
+    
+    return ' '.join(text.replace('\n', '').split())
+    
+
+def cosine_similarity(arr1: np.ndarray, arr2: np.ndarray) -> float:
+    '''arr1 and arr2 should be 1D vectors'''
+    return (np.dot(arr1, arr2) / (np.linalg.norm(arr1) * np.linalg.norm(arr2)))
+    
+def fetch_postings_text(database_url: str) -> List[str]:
     '''
     Returns a list of the postings' body text from the database
     '''
@@ -22,18 +44,43 @@ def embed_text(text: str, word_reduction_type: str='sum_last_four') -> np.ndarra
     - sum_last_four: Sum the outputs of the last four layers
     - concat_last_four: Concatenate the outputs of the last four layers in one long vector (NOT YET IMPLEMENTED)
     - last_layer: Use only the lasy layer as the embedding (NOT YET IMPLEMENTED)
+    
+    Returns embeddings as torch.Tensor: Size (num_tokens x 768); one vector for each word.
     '''
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokens_t = torch.tensor([tokenizer.encode(text)])
+    text = _clean_text(text)
+    tokenizer = AutoTokenizer.from_pretrained(_EMBED_MODEL)
+    tokens_t = torch.tensor([tokenizer.encode(text)[:512]])
+    
     segment_t = torch.tensor([[1] * tokens_t.shape[1]])
-    model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+    model = AutoModel.from_pretrained(_EMBED_MODEL, add_pooling_layer=False, output_hidden_states=True)
     model.eval()
+#     print(tokenizer.encode(text))
+#     print(model(**tokenizer(text, return_tensors='pt')))
+
     with torch.no_grad():
-        out = model(tokens_t, segment_t)
-        hidden_states = out[2]
+#         out = model(tokens_t)
+        out = model(**tokenizer(text, return_tensors='pt'))
+#         pooling_state = out[1]
+        hidden_states = out[1]
         hidden_states = torch.cat(hidden_states).permute(1, 0, 2)
         
-        if reduction_type == 'sum_last_four':
-            embeddings = hidden_states[:, -4:].sum(dim=1)
-        
-        return embeddings
+        if word_reduction_type == 'sum_last_four':
+            embeddings = hidden_states[:, -4:].mean(dim=1)
+        return embeddings.numpy()
+
+    
+def doc_sim_score(emb1: np.ndarray, emb2: np.ndarray, comp_type: str='mean_embeddings') -> np.ndarray:
+    '''
+    Get the similarity score between a job posting and a resume (order does not matter). Parameters
+    emb1 and emb2 are token-level embeddings as returned by `embed_text`. There are several
+    options to compare (comp_type):
+    
+    - mean_embeddings: Average token embeddings for each document, then take the cosine similarity
+    - others????
+    '''
+    
+    if comp_type == 'mean_embeddings':
+        mean1, mean2 = emb1.mean(axis=0), emb2.mean(axis=0)
+        return cosine_similarity(mean1, mean2)
+    else:
+        raise ValueError('Please provide a valid value for argument `mean_embeddings`')
